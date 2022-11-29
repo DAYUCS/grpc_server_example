c2c101c76f571b6414e37bb6bbdebdf410b015a1
Dayu Bian <dayub@chinasystems.com>
2022-11-29 15:53:52 +0800

    bugfix

--- client/client.js
+++ client/client.js
@@ -1,17 +1,18 @@
-const { HelloRequest, HelloReply } = require('./helloworld_pb.js');
+const { HelloRequest, Person } = require('./helloworld_pb.js');
 const { GreeterClient } = require('./helloworld_grpc_web_pb.js');
 
 XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 //Apisix grpc-web
-var client = new GreeterClient("http://10.39.101.186:9080/grpc/web/");
+var client = new GreeterClient("http://10.39.101.186:9080/grpc/web");
 
 //Make an unary RPC call
 var request = new HelloRequest();
 request.setName("Alex");
-//request.setItemsList(["1st item", "2nd item"]);
-//request.setGender(2);
-//request.setPerson({ "name": "Alex", "age": 20 });
+request.setItemsList(["1st item", "2nd item"]);
+request.setGender(2);
+var person = new Person({ name: "Alex", age: 20 });
+request.setPerson(person);
 client.sayHello(request, {}, (err, respose) => {
     if (err) {
         console.log(err.code);
