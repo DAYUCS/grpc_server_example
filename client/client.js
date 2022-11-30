bbfd36f381cac85d4e1fb9286698f2e00411b826
Dayu Bian <dayub@chinasystems.com>
2022-11-30 13:25:13 +0800

    Finish grpc-web client function

--- client/client.js
+++ client/client.js
@@ -1,10 +1,8 @@
 const { HelloRequest, Person } = require('./helloworld_pb.js');
 const { GreeterClient } = require('./helloworld_grpc_web_pb.js');
 
-XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-//Apisix grpc-web
-var client = new GreeterClient("http://10.39.101.186:9080/grpc/web");
+//Enovy grpc-web
+var client = new GreeterClient('http://10.39.107.102:8080', null, null);
 
 //Make an unary RPC call
 var request = new HelloRequest();
@@ -13,11 +11,22 @@ request.setItemsList(["1st item", "2nd item"]);
 request.setGender(2);
 var person = new Person({ name: "Alex", age: 20 });
 request.setPerson(person);
-client.sayHello(request, {}, (err, respose) => {
+
+client.sayHello(request, {}, (err, response) => {
     if (err) {
-        console.log(err.code);
-        console.log(err.message);
+        console.log(`Unexpected error for sayHello: code = ${err.code}` +
+            `, message = "${err.message}"`);
     } else {
         console.log(response.getMessage());
     }
+});
+
+//Server stream RPC call
+var stream = client.sayHelloServerStream(request, {});
+stream.on('data', (response) => {
+    console.log(response.getMessage());
+});
+stream.on('error', (err) => {
+    console.log(`Unexpected stream error: code = ${err.code}` +
+        `, message = "${err.message}"`);
 });
